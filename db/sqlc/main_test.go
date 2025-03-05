bdcbe83628798487c56b2484d64a4d081dfef73c
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2025-03-05 07:09:37 +0200

    fix: Add CI config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,10 +14,25 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../..")
+	configPaths := []string{
+		"../..",
+		"../../.github/workflows",
+	}
+
+	var config util.Config
+	var err error
+
+	for _, path := range configPaths {
+		config, err = util.LoadConfig(path)
+		if err == nil {
+			break
+		}
+	}
+
 	if err != nil {
 		log.Fatalf("cannot load config: %v", err)
 	}
+
 	testDB, err = sql.Open(config.DBDeriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the DB: ", err)
